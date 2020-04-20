78197fb63bf54a896330645819e934a858b5b21c
Andy <alayers2@gmail.com>
2020-04-20 13:17:07 -0500

    update app.json, and simplify main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,37 @@
 
 #[macro_use] extern crate rocket;
 
-use rocket::http::RawStr;
+use rocket::Rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/other")]
-fn other() -> &'static str {
-    "Hello, other world!"
-}
-
-#[get("/<name>")]
-fn name(name: &RawStr) -> String {
-    format!("Hello, {}!", name.as_str())
-}
-
-mod outside {
-    #[get("/outside")]
-    pub fn world() -> &'static str {
-        "Hello, outside world!"
-    }
+fn build_rocket() -> Rocket {
+    rocket::ignite()
+    .mount("/", routes![index])
 }
 
 fn main() {
     rocket::ignite()
     .mount("/", routes![index])
-    .mount("/hello", routes![other, outside::world, name])
     .launch();
 }
+
+
+#[cfg(test)]
+ mod test {
+     use super::build_rocket;
+     use rocket::local::Client;
+     use rocket::http::{Status};
+
+     #[test]
+     fn hello_world() {
+         let client = Client::new(build_rocket()).expect("valid rocket instance");
+         let mut response = client.get("/").dispatch();
+
+         assert_eq!(response.status(), Status::Ok);
+         assert_eq!(response.body_string(), Some("Hello, world!".into()));
+     }
+ }
